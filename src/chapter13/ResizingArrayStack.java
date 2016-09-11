3c621fe976fbd69356b5a2ad958dda425bd6281d
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-11 22:04:22 +0800

    implements stack data structure

--- src/chapter13/ResizingArrayStack.java
+++ src/chapter13/ResizingArrayStack.java
@@ -11,21 +11,54 @@ public class ResizingArrayStack<Item> implements Iterable<Item> {
     private Item[] a = (Item[]) new Object[1];
     private int N = 0;
 
+    /**
+     * 获取栈的大小
+     * @return
+     */
     public int size() {
         return N;
     }
 
+    /**
+     * 判断栈是否为空
+     * @return
+     */
     public boolean isEmpty() {
         return N == 0;
     }
 
+    /**
+     * 压栈
+     * @param item
+     * @return
+     */
     public Item push(Item item) {
         a[N++] = item;
+        if (N == a.length) {
+            resize(2 * a.length);
+        }
         return item;
     }
 
+    /**
+     * 出栈
+     * @return
+     */
     public Item pop() {
-        return a[--N];
+        Item item = a[--N];
+        a[N] = null;
+        if (N > 0 && N == a.length / 4) {
+            resize(a.length / 2);
+        }
+        return item;
+    }
+
+    private void resize(int capacity) {
+        Item[] arr = (Item[]) new Object[capacity];
+        int length = a.length;
+        for (int i = 0; i < length; i++) {
+            arr[i] = a[i];
+        }
     }
 
     @Override
