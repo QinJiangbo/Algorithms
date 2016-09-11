59f024e3b1eb7af85780296d31941b9df0cc352b
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-11 21:47:48 +0800

    implements stack data structure

--- src/chapter13/ResizingArrayStack.java
+++ src/chapter13/ResizingArrayStack.java
@@ -1,8 +1,28 @@
 package chapter13;
 
+import java.util.Iterator;
+
 /**
  * Date: 9/11/16
  * Author: qinjiangbo@github.io
  */
-public class ResizingArrayStack {
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Item next() {
+            return null;
+        }
+    }
 }
