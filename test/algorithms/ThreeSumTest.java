f549037f62f55ef6ff91e9893e544252bcd9b150
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-29 23:40:30 +0800

    add ThreeSum  test cases

---
+++ test/algorithms/ThreeSumTest.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+import org.junit.Test;
+
+/**
+ * Date: 29/09/2016
+ * Author: qinjiangbo@github.io
+ */
+public class ThreeSumTest {
+
+    @Test
+    public void testThreeSum() {
+        int[] a = new int[]{11, -4, -7, -8, 13, -5, -4, 9, 12, 13, -21};
+        System.out.println(ThreeSum.count(a));
+    }
+}
