85256dc57a5e09010d457c1e7f45318ffcf8b632
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-29 23:37:26 +0800

    add ThreeSum class

---
+++ src/algorithms/ThreeSum.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+/**
+ * Date: 29/09/2016
+ * Author: qinjiangbo@github.io
+ */
+public class ThreeSum {
+
+    /**
+     * 统计一个数组中和为0的三个数的组合个数
+     *
+     * @param a
+     * @return
+     */
+    public static int count(int[] a) {
+        int N = a.length;
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+}
