80701386199746783763e364fe5ed1d94d2a8ddc
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-30 23:26:54 +0800

    add TwoSum  test cases

--- test/algorithms/TwoSumTest.java
+++ test/algorithms/TwoSumTest.java
@@ -10,6 +10,7 @@ public class TwoSumTest {
 
     @Test
     public void testTwoSum() {
-
+        int[] a = new int[]{-1, -7, 8, -9, 6, 7, 1, 10, 4, -55, 43, -5, -6, -7};
+        System.out.println(TwoSum.count(a));
     }
 }
