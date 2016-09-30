4e6a443bccd8cfe144ee81dd5964095ad2776a8d
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-30 23:23:11 +0800

    add TwoSum  test cases

---
+++ src/algorithms/TwoSum.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+/**
+ * Date: 30/09/2016
+ * Author: qinjiangbo@github.io
+ */
+public class TwoSum {
+
+    /**
+     * 统计数组中互为相反数的两个数
+     *
+     * @param a
+     * @return
+     */
+    public static int count(int[] a) {
+
+        return 0;
+    }
+
+}
