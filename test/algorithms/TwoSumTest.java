77542b3ec267b98b5e96ff6220d4af4f5e07efce
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-30 23:25:36 +0800

    add TwoSum  test cases

---
+++ test/algorithms/TwoSumTest.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+import org.junit.Test;
+
+/**
+ * Date: 30/09/2016
+ * Author: qinjiangbo@github.io
+ */
+public class TwoSumTest {
+
+    @Test
+    public void testTwoSum() {
+
+    }
+}
