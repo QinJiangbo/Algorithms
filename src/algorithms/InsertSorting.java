310bc4fe142c9261512279ae1ee436001bb24ffb
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-10-02 23:30:23 +0800

    add Select Sorting method

---
+++ src/algorithms/InsertSorting.java
@@ -0,0 +1,8 @@
+package algorithms;
+
+/**
+ * Date: 02/10/2016
+ * Author: qinjiangbo@github.io
+ */
+public class InsertSorting {
+}
---
+++ src/algorithms/SelectSorting.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+/**
+ * Date: 02/10/2016
+ * Author: qinjiangbo@github.io
+ */
+public class SelectSorting {
+
+    /**
+     * Sort the array with selective sorting method
+     *
+     * @param a
+     */
+    public static void sort(int[] a) {
+
+    }
+}
