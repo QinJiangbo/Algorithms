e77725337c97c0230f46d66a5e415eded6cd420a
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-19 17:52:39 +0800

    adding bubble sorting algorithm test case

--- src/algorithms/sortings/BubbleSorting.java
+++ src/algorithms/sortings/BubbleSorting.java
@@ -1,10 +1,18 @@
 package algorithms.sortings;
 
+import java.util.Arrays;
+
 /**
  * @date: 18/01/2017 10:52 PM
  * @author: qinjiangbo@github.io
  */
 public class BubbleSorting extends BaseSorting {
+
+    public static void main(String[] args) {
+        int[] a = {11, 4, 6, 3, 9, 4, 13, 18};
+        new BubbleSorting().sort(a);
+        System.out.printf(Arrays.toString(a));
+    }
 
     @Override
     public void sort(int[] a) {
