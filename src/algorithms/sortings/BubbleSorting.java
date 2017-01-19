018e97a16ceee24b5f3457e79cecdc9d4b796103
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-19 17:49:50 +0800

    implementing bubble sorting algorithm

--- src/algorithms/sortings/BubbleSorting.java
+++ src/algorithms/sortings/BubbleSorting.java
@@ -8,6 +8,12 @@ public class BubbleSorting extends BaseSorting {
 
     @Override
     public void sort(int[] a) {
-
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    swap(i, j, a);
+                }
+            }
+        }
     }
 }
