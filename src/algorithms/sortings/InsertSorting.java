e720841581f42aac52e1641b4e9e25d586f696db
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-20 20:26:39 +0800

    add Insert Sorting algorithms

--- src/algorithms/sortings/InsertSorting.java
+++ src/algorithms/sortings/InsertSorting.java
@@ -1,15 +1,27 @@
 package algorithms.sortings;
 
+import java.util.Arrays;
+
 /**
  * Date: 02/10/2016
  * Author: qinjiangbo@github.io
  */
 public class InsertSorting extends BaseSorting {
 
+    public static void main(String[] args) {
+        int[] a = {11, 4, 6, 3, 9, 4, 13, 18};
+        new InsertSorting().sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
     @Override
     public void sort(int[] a) {
         for (int i = 0; i < a.length; i++) {
-
+            for (int j = i; j > 0; j--) {
+                if (a[j] < a[j - 1]) {
+                    swap(j, j - 1, a);
+                }
+            }
         }
     }
 
