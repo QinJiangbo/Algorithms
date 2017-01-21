ba8636be49913883e088b2b60ec608cb28ba56e2
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-21 23:16:12 +0800

    add Quick Sorting algorithms

---
+++ src/algorithms/sortings/QuickSorting.java
@@ -0,0 +1,13 @@
+package algorithms.sortings;
+
+/**
+ * @date: 21/01/2017 11:15 PM
+ * @author: qinjiangbo@github.io
+ */
+public class QuickSorting extends BaseSorting {
+
+    @Override
+    public void sort(int[] a) {
+
+    }
+}
