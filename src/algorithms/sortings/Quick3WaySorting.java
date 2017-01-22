d63721eb5d276bb576b33c28caaa9adad84a8155
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-22 22:24:09 +0800

    add Quick 3 Way Sorting algorithms

---
+++ src/algorithms/sortings/Quick3WaySorting.java
@@ -0,0 +1,13 @@
+package algorithms.sortings;
+
+/**
+ * @date: 22/01/2017 10:23 PM
+ * @author: qinjiangbo@github.io
+ */
+public class Quick3WaySorting extends BaseSorting {
+
+    @Override
+    public void sort(int[] a) {
+
+    }
+}
