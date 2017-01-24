f0e2462d90070f00adc6a65dffc477d304d0a096
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-24 15:36:33 +0800

    refactor the class name

--- src/algorithms/sortings/Quick3Way.java
+++ src/algorithms/sortings/Quick3Way.java
@@ -4,7 +4,7 @@ package algorithms.sortings;
  * @date: 22/01/2017 10:23 PM
  * @author: qinjiangbo@github.io
  */
-public class Quick3WaySorting extends BaseSorting {
+public class Quick3Way extends BaseSorting {
 
     @Override
     public void sort(int[] a) {
