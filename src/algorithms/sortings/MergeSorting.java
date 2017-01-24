dd8796fb6eb122ecc0ddda3d2d08e50611312d80
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-24 17:25:20 +0800

    add merge method for merge sorting algorithm

--- src/algorithms/sortings/MergeSorting.java
+++ src/algorithms/sortings/MergeSorting.java
@@ -10,4 +10,15 @@ public class MergeSorting extends BaseSorting {
     public void sort(int[] a) {
 
     }
+
+    /**
+     * merge method for combining two arrays into one
+     *
+     * @param a  array
+     * @param lo low index
+     * @param hi high index
+     */
+    private void merge(int[] a, int lo, int hi) {
+
+    }
 }
