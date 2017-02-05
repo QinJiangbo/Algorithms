6d5f3abb613b4184cc6a8203b7cb668842027236
qinjiangbo <1105351275@qq.com>
2017-02-05 23:37:28 +0800

    add comments for Shell Sorting Algorithms

--- src/algorithms/sortings/ShellSorting.java
+++ src/algorithms/sortings/ShellSorting.java
@@ -19,7 +19,7 @@ public class ShellSorting extends BaseSorting {
         int N = a.length;
         int h = 1;
         while (h < N/3) {
-            h = 3*h + 1;
+            h = 3*h + 1; // 1, 4, 13, 40, 121, 364, 1093
         }
         while (h >= 1) {
             for (int i = h; i < N; i++) {
