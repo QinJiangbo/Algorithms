6888121469c1f092a5067175dd066a9c207d0bf0
qinjiangbo <1105351275@qq.com>
2017-02-05 23:34:12 +0800

    add Shell Sorting Algorithms

--- src/algorithms/sortings/ShellSorting.java
+++ src/algorithms/sortings/ShellSorting.java
@@ -8,6 +8,20 @@ public class ShellSorting extends BaseSorting {
 
     @Override
     public void sort(int[] a) {
-
+        int N = a.length;
+        int h = 1;
+        while (h < N/3) {
+            h = 3*h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h; j =- h) {
+                    if (a[j] < a[j-h]) {
+                        swap(j, j-h, a);
+                    }
+                }
+            }
+            h = h/3;
+        }
     }
 }
