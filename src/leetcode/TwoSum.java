1cf59f09c5673daeddf676d0910fa673513b521c
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-02-24 01:13:35 +0800

    optimize code
    Signed-off-by: QinJiangbo <QinJiangbo@users.noreply.github.com>

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -19,7 +19,7 @@ public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 result[1] = i;
